466f4c2885692fa3dde8b2bb37c010829b34cac0
Adamakos <adamhohmann@gmail.com>
2021-03-24 17:29:24 +0100

    Add radiocharts.java

--- src/main/java/com/codecool/database/RadioCharts.java
+++ src/main/java/com/codecool/database/RadioCharts.java
@@ -1,6 +1,56 @@
 package com.codecool.database;
 
 
+import java.sql.*;
+
 public class RadioCharts {
-    //TODO: implement this class
+
+    private final String url;
+    private final String user;
+    private final String password;
+
+    public RadioCharts(String url, String user, String password) {
+        this.url = url;
+        this.user = user;
+        this.password = password;
+    }
+
+    public String getMostPlayedSong() {
+        String name = null;
+        String query = "SELECT song FROM music_broadcast WHERE times_aired = (SELECT MAX(times_aired) FROM music_broadcast)";
+        try (Connection connection = getConnection()) {
+            PreparedStatement statement = connection.prepareStatement(query);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) name = resultSet.getString("song");
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        if (name == null) return "";
+        return name;
+    }
+
+    public String getMostActiveArtist() {
+        String name = null;
+        String query = "SELECT TOP 1 artist FROM music_broadcast GROUP BY artist ORDER BY COUNT(song) DESC";
+        try (Connection connection = getConnection()) {
+            PreparedStatement statement = connection.prepareStatement(query);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) name = resultSet.getString("artist");
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        if (name == null) return "";
+        return name;
+    }
+
+    private Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(url, user, password);
+        } catch (SQLException e) {
+            System.err.println("Database unable to connect.");
+        }
+        return connection;
+    }
+
 }
